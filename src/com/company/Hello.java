e3d41bc4083b1987786cd01024fbc4f46fc59ece
konst <qwertyuiop12345jomh@gmail.com>
2021-02-15 19:47:54 +0300

    g

--- src/com/company/Hello.java
+++ src/com/company/Hello.java
@@ -1,7 +1,16 @@
 package com.company;
 
+
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.Thread.sleep;
+
 public class Hello {
     public static void Hello(){
         System.out.println("Hello, World!");
+        try {
+            sleep(2000);
+        } catch (Exception e){};
+
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,19 @@
 package com.company;
 
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 
     public static void main(String[] args) {
         MyExecutor myExecutor = new MyExecutor(3);
         myExecutor.submit(() -> Hello.Hello());
+        myExecutor.submit(() -> Hello.Hello());
+        myExecutor.submit(() -> Hello.Hello());
+        myExecutor.submit(() -> Hello.Hello());
+        myExecutor.shutdown();
+        try {
+            System.out.println(myExecutor.awaitTermination(1000, TimeUnit.MILLISECONDS));
+        } catch (Exception e){};
+
     }
 }
