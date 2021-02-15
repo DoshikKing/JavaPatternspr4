13ea1101e7d96e14df2b9385cf5da94e2809c7ee
konst <qwertyuiop12345jomh@gmail.com>
2021-02-15 19:53:16 +0300

    g

--- src/com/company/Hello.java
+++ src/com/company/Hello.java
@@ -6,8 +6,8 @@ import java.util.concurrent.TimeUnit;
 import static java.lang.Thread.sleep;
 
 public class Hello {
-    public static void Hello(){
-        System.out.println("Hello, World!");
+    public static void Hello(String index){
+        System.out.println("Hello, World!" + index);
         try {
             sleep(2000);
         } catch (Exception e){};
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,21 @@
 package com.company;
 
 import java.util.concurrent.TimeUnit;
+import static java.lang.Thread.sleep;
 
 public class Main {
 
     public static void main(String[] args) {
         MyExecutor myExecutor = new MyExecutor(3);
-        myExecutor.submit(() -> Hello.Hello());
-        myExecutor.submit(() -> Hello.Hello());
-        myExecutor.submit(() -> Hello.Hello());
-        myExecutor.submit(() -> Hello.Hello());
+        myExecutor.submit(() -> Hello.Hello("1"));
+        myExecutor.submit(() -> Hello.Hello("2"));
+        myExecutor.submit(() -> Hello.Hello("3"));
+        myExecutor.submit(() -> Hello.Hello("4"));
+
         myExecutor.shutdown();
         try {
             System.out.println(myExecutor.awaitTermination(1000, TimeUnit.MILLISECONDS));
+            sleep(2000);
         } catch (Exception e){};
 
     }
